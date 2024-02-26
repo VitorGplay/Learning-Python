242d227b57e60112ad39296843b0e5dd4f18b348
VitorGplay <vocconhece@gmail.com>
2024-02-26 14:03:13 -0300

    Um pouco mais organizado!

--- Automation/Easysheet/app.py
+++ Automation/Easysheet/app.py
@@ -5,7 +5,8 @@ planilha = openpyxl.load_workbook('planilhas/planilha_alunos.xlsx')
 folha = planilha['Sheet1']
 
 for indice, linha in enumerate(folha.iter_rows(min_row=2)):
-    #Acessar cada celula que precisamos
+
+    #Acessar cada celula
     nome_curso = linha[0].value
     nome_participante = linha[1].value
     tipo_participacao = linha[2].value
@@ -14,15 +15,15 @@ for indice, linha in enumerate(folha.iter_rows(min_row=2)):
     data_inicio = linha[3].value
     data_fim = linha[4].value
 
-    data_emicao = linha[6].value
+    data_emissao = linha[6].value
 
     #Transferir os dados da planilha para a imagem do certificado
     #Fonte a ser usada
-    fonte_nome = ImageFont.truetype('./tahomabd.ttf', 90)
-    fonte_geral = ImageFont.truetype('./tahoma.ttf', 80)
-    fonte_data = ImageFont.truetype('./tahoma.ttf', 55)
+    fonte_nome = ImageFont.truetype('./fontes/tahomabd.ttf', 90)
+    fonte_geral = ImageFont.truetype('./fontes/tahoma.ttf', 80)
+    fonte_data = ImageFont.truetype('./fontes/tahoma.ttf', 55)
 
-
+    #Escrever as informações da planilha na imagem do certificado.
     image = Image.open('./certificado_padrao.jpg')
     desenhar = ImageDraw.Draw(image)
     desenhar.text((1015, 827), nome_participante, fill='black', font=fonte_nome)
@@ -33,12 +34,12 @@ for indice, linha in enumerate(folha.iter_rows(min_row=2)):
     desenhar.text((700, 1750), str(data_inicio), fill='black', font=fonte_geral)
     desenhar.text((700, 1900), str(data_fim), fill='black', font=fonte_geral)
 
-    desenhar.text((2150, 1900), str(data_emicao), fill='black', font=fonte_geral)
+    desenhar.text((2150, 1900), str(data_emissao), fill='black', font=fonte_geral)
 
 
 
-
-    image.save(f'./{indice} {nome_participante} certificado.png')
+    #Salvar uma imagem do certificado modificado para cada celula lida.
+    image.save(f'./certificados/{indice} {nome_participante} certificado.png')
 
 
 
---
+++ Automation/Easysheet/readme.txt
@@ -0,0 +1,20 @@
+Description:
+
+This Python application automates the process of transferring information from an Excel spreadsheet to a certificate image. Instead of manually copying and pasting data from the spreadsheet to the certificate, this program reads the data from the spreadsheet and "writes" it onto the certificate image automatically.
+
+Features:
+
+Reads data from an Excel spreadsheet containing information such as course name, participant name, type of participation, duration, start date, end date, and issuance date.
+Copies this information onto a certificate image, eliminating the need for manual entry.
+Streamlines the process and reduces human error.
+Currently, configuration settings are managed within the code.
+Future plans include developing a user-friendly interface where users can select the spreadsheet and certificate image.
+The ultimate goal is to compile the application into an executable (exe) file for intuitive use.
+
+How to Use:
+
+Run the program.
+The program automatically fetches data from the designated Excel spreadsheet.
+It then transfers this data onto the certificate image.
+The resulting certificate images are saved automatically.
+This application simplifies the administrative tasks associated with issuing certificates for courses or events, making the process more efficient and less prone to errors.
